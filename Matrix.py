39d8f128d392ae7ec4868de2cdbc8bbfe229dd0f
Jawad Makhoul <jawad.makhol@gmail.com>
2024-11-26 13:33:49 +0200

    Task 1: Recent Call Logs

--- Matrix.py
+++ Matrix.py
@@ -1,1 +1,7 @@
-#Matrix
+def recent_calls(calls, k): # Recent Call Logs
+    return calls[-k:] if k > 0 else []
+
+print(recent_calls([1001,1002,1003,1004], 3))
+print(recent_calls([1,2], 3))
+print(recent_calls([1, 2,3,4,5], 2))
+print(recent_calls([],5))
