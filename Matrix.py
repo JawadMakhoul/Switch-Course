8a92404af561c4047702fe78c5cba0b727774168
Jawad Makhoul <jawad.makhol@gmail.com>
2024-11-26 14:06:10 +0200

    Task 2: Inventory Shelf Arrangement

--- Matrix.py
+++ Matrix.py
@@ -1,3 +1,4 @@
+# Task 1
 def recent_calls(calls, k): # Recent Call Logs
     return calls[-k:] if k > 0 else []
 
@@ -5,3 +6,20 @@ print(recent_calls([1001,1002,1003,1004], 3))
 print(recent_calls([1,2], 3))
 print(recent_calls([1, 2,3,4,5], 2))
 print(recent_calls([],5))
+
+################################################################
+
+# Task 2
+def inventory_shelf_arrangement(shelfList):
+
+    newList=[]
+    stack = []
+    for cell in shelfList:
+        stack.append(cell)
+
+    while stack:
+        newList.append(stack.pop())
+    
+    return newList
+
+print(inventory_shelf_arrangement([101,102,103,104]))
