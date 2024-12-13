5f64bd01fb131c409119f4fb20dcbdf29d050c2e
JawadMakhoul <jawad.makhol@gmail.com>
2024-12-13 20:45:13 +0200

    Common-elements

--- Algorithmic-Strategies/Common-elements.py
+++ Algorithmic-Strategies/Common-elements.py
@@ -1,1 +1,16 @@
+arr1 =[1,2,2,3,3]
+arr2 = [2,2,3,3,8]
+result = []
+
+for i in arr1:
+    for j in arr2:
+        if i == j:
+            result.append(i)
+
+for i in range(len(result)-1):
+    for j in range(1, len(result)):
+        if i == j:
+            result.pop(j)
+            
+print(result)
 
