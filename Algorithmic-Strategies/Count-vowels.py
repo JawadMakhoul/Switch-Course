8abac3d80793bfe5f2b08245bd103b9fb2c09c92
JawadMakhoul <jawad.makhol@gmail.com>
2024-12-14 19:32:02 +0200

    Count-vowels

---
+++ Algorithmic-Strategies/Count-vowels.py
@@ -0,0 +1,13 @@
+str = "zAdaeioou"
+
+vowels = ['A','E','I','O','U','a','e','i','o','u']
+VowelsCount = 0 
+ConstCount = 0
+for c in str:
+    for v in vowels:
+        if c == v :
+            VowelsCount+=1
+    
+ConstCount = len(str) - VowelsCount
+print("Vowels: ",VowelsCount)
+print("Consonants: ", ConstCount)
