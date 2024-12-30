5ef0f029215e7d2d7988cd9c1afb923a64acaf0b
JawadMakhoul <jawad.makhol@gmail.com>
2024-12-30 10:14:44 +0200

    Count chars by length

--- Algorithmic-Strategies/chars-to-length.py
+++ Algorithmic-Strategies/chars-to-length.py
@@ -8,8 +8,23 @@ def chars_to_length(input_str):
             char_counter = 1
         else:
             char_counter += 1 
-
+    
     return finalString
 
+def first_odd_counter(input_str):
+    
+    for i in range(0, len(input_str) -1, 2):
+        char = input_str[i]
+        num = int(input_str[i+1])
+
+        if num % 2 != 0:
+            return char
+        
+        return None
+
+
+
 user_input = input("Enter a String: ")
-print(chars_to_length(user_input))
+chars_to_length_result = chars_to_length(user_input)
+print(chars_to_length_result)
+print("First odd counter is: " ,first_odd_counter(chars_to_length_result))
